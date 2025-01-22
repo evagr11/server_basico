65b35b7e26a7cf52f30956f53c74b299743b9081
eva <evagallardor@icloud.com>
2025-01-22 11:08:31 +0100

    feat: uncommitted changues

--- server.js
+++ server.js
@@ -2,12 +2,32 @@ const express = require('express');
 
 const app = express();
 
-function holaMundo(request, response){
-    console.log("Hola mundo") // Esto se ejecuta en el servidor
-    response.send("Hola mundo") // Esto se devuelve al cliente
+const users =[
+    {
+        "username": "admin",
+        "password": "admin",
+        "email": "admin@gmail.com",
+        "status": "active"
+    },
+    {
+        "username": "user1",
+        "password": "user1",
+        "email": "user1@gmail.com",
+        "status": "active"
+    }
+]
+
+function getUsers(request, response){
+    response.json(users) // Esto se devuelve al cliente
 }
 
-app.get("/", holaMundo)
+//app.get("/users", getUsers)
+app.get("/", (req, response) => {
+    response.send("Hola mundo")
+})
+app.get("/users", (req, res) =>{
+    res.json(users)
+})
 
 app.listen(3000, () => {
     console.log("El servidor está funcionando en el puerto 3000");
